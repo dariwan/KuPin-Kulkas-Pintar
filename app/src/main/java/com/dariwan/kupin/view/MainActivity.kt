7ed644586222dd2af30e52c2d5c97a65337045fe
dariwan <dariwan1607@gmail.com>
2024-04-08 22:11:56 +0700

    add: bottom nav bar

--- app/src/main/java/com/dariwan/kupin/view/MainActivity.kt
+++ app/src/main/java/com/dariwan/kupin/view/MainActivity.kt
@@ -1,11 +1,13 @@
-package com.dariwan.kupin
+package com.dariwan.kupin.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dariwan.kupin.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
