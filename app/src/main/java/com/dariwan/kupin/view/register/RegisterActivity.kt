7bf44048019a8152a2e42e8a0b06efa735ec3e41
dariwan <dariwan1607@gmail.com>
2024-04-12 19:59:28 +0700

    add: view register

---
+++ app/src/main/java/com/dariwan/kupin/view/register/RegisterActivity.kt
@@ -0,0 +1,15 @@
+package com.dariwan.kupin.view.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dariwan.kupin.R
+import com.dariwan.kupin.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
