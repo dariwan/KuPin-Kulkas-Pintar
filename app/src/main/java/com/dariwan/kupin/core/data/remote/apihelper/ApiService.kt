ce2c9af93a22c07543135d0057debc3cd8dfc856
dariwan <dariwan1607@gmail.com>
2024-05-21 09:01:03 +0700

    fix: bug fix

---
+++ app/src/main/java/com/dariwan/kupin/core/data/remote/apihelper/ApiService.kt
@@ -0,0 +1,15 @@
+package com.dariwan.kupin.core.data.remote.apihelper
+
+import com.dariwan.kupin.core.data.remote.request.MaterialRequest
+import com.dariwan.kupin.core.data.remote.response.RecommendationResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface ApiService {
+    @POST("")
+    fun getRecommendation(
+        @Body material: MaterialRequest,
+    ): Call<RecommendationResponse>
+
+}
